4aff1aec1b232e6ed58abbc8fd9e29be87cc69f2
Sumit <sumit.sharma@remotestate.com>
2023-08-25 11:28:38 +0530

    working c1

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
-                    // change first commit in test
+                    // change first commit in test working 1
                 }
             }
         }
